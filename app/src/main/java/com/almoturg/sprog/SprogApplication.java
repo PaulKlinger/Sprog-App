1862e69f259ac28953bf5641c4bb47dceb0b1f06
Paul Klinger <klinger.paul@gmail.com>
2017-01-31 20:53:11 +0100

    enabled automatic google analytics tracking of crashes

--- app/src/main/java/com/almoturg/sprog/SprogApplication.java
+++ app/src/main/java/com/almoturg/sprog/SprogApplication.java
@@ -48,7 +48,7 @@ public class SprogApplication extends Application {
         if (mTracker == null) {
             GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
             // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
-            mTracker = analytics.newTracker(R.xml.global_tracker);
+            mTracker = analytics.newTracker(R.xml.app_tracker);
         }
         return mTracker;
     }
