29965dac80bb9388b368b741d2c131a6daae4da0
Paul Klinger <klinger.paul@gmail.com>
2017-02-13 18:38:11 +0100

    fixed event tracking of stats page

--- app/src/main/java/com/almoturg/sprog/presenter/MainPresenter.java
+++ app/src/main/java/com/almoturg/sprog/presenter/MainPresenter.java
@@ -246,7 +246,7 @@ public class MainPresenter {
     }
 
     public void optionStats() {
-        activity.trackEvent("openStats", null, null);
+        activity.trackEvent("openStats", "", null);
         activity.launchStats();
     }
 
